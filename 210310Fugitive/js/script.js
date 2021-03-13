e5c295ea0bcf03a9826e7c8dadfe066d69169ccb
jd053303 <64065652+jd053303@users.noreply.github.com>
2021-03-13 23:26:17 +0900

    Add files via upload

---
+++ 210310Fugitive/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    //역할 팝업
+    $(".go").click(function(){
+        $(".role").show();
+      });
+      $(".clo").click(function(){
+          $(".role").hide();
+      });
+    
+    //룰 팝업
+    $(".ru").click(function(){
+        $(".rule").show();
+      });
+      $(".clo").click(function(){
+          $(".rule").hide();
+      });
+
+      
+});``
